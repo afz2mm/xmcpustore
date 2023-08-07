89f3d11e27dc7239bf8545398362e25972eecb82
afz2mm <141427780+afz2mm@users.noreply.github.com>
2023-08-07 19:23:56 +0545

    Update dw1.sh

--- tpp/dw1.sh
+++ tpp/dw1.sh
@@ -1,1 +1,1 @@
-! wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+! wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz
